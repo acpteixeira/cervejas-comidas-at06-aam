4fcd1bb6a6d5c4085dec398f10abe6fbf38b2ef2
acpteixeira <anacomputacao@gmail.com>
2019-06-27 21:42:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# at-06-aam
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/at-06-aam)
---
+++ pages/home/home.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  cerveja = '';
+
+  private combinacoes =
+    {
+      "Pale Ale": ["Pizza Calabresa", "Churrasco", "Salgadinhos de Pacote"],
+      "Lager": ["Pizza Margerita", "Salgadinhos de Pacote"],
+      "Pilsen": ["Asinha de Frango", "Fettucine Alfredo", "Batata Frita", "Linguiça apimentada"],
+      "IPA": ["Churrasco de picanha", "Torresmo", "Chessbacon", "Feijoada Gorda"],
+      "Red Ale": ["Churrasco", "Frango Assado"],
+      "Bock": ["Costela de porco grelhada", "Lombo de porco assado", "Javali"],
+      "Weiss (trigo)": ["Muqueca", "Peixe Assado", "Ceviche"]
+    }
+
+
+  public cervejasUnicas = Object.keys(this.combinacoes);
+
+  get combinacao() {
+    return this.combinacoes[this.cerveja];
+  }
+}
